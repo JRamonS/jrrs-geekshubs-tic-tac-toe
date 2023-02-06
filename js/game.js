2d338752b8b633c8bffbd3f1b746d6b3ca74023b
Ramon <ramon.r.santana@gmail.com>
2023-02-06 20:40:05 +0100

    creación-help

---
+++ js/game.js
@@ -0,0 +1,5 @@
+let player1info = document.getElementById("player1");
+player1info.innerHTML = sessionStorage.getItem("Name player 1");
+
+let player2info = document.getElementById("player2");
+player2info.innerHTML = sessionStorage.getItem("Name player 2");

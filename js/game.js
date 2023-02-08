6c1e725a97d09a10369fcc1f5d7e81d82c2a5d2c
Ramon <ramon.r.santana@gmail.com>
2023-02-08 16:55:49 +0100

    tabla JS funciones

--- js/game.js
+++ js/game.js
@@ -1,5 +1,17 @@
-let player1info = document.getElementById("player1");
-player1info.innerHTML = sessionStorage.getItem("Name player 1");
-
-let player2info = document.getElementById("player2");
-player2info.innerHTML = sessionStorage.getItem("Name player 2");
+const boxList = document.querySelectorAll('.boardContainer .box')
+let turn = true;
+const movesArray = new Array(9).fill(null)
+boxList.forEach(function (box, index) {
+    box.addEventListener('click', function () {
+        if(turn){
+            box.textContent = 'X'
+        }else{
+            box.textContent = 'O'
+        }
+        movesArray[index] = turn
+        if (movesArray [0] == movesArray[1] &&
+            movesArray [1] == movesArray[2] &&
+            movesArray[0] != null)
+        turn = !turn
+    }, {once : true})
+})

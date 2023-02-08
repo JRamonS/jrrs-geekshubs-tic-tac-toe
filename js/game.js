d2f562d57506d2604c8c502aaf4f27e996fcda5f
Ramon <ramon.r.santana@gmail.com>
2023-02-08 17:50:48 +0100

    creando las variables para marcar la X o el O en el tablero

--- js/game.js
+++ js/game.js
@@ -1,17 +1,19 @@
-const boxList = document.querySelectorAll('.boardContainer .box')
-let turn = true;
-const movesArray = new Array(9).fill(null)
-boxList.forEach(function (box, index) {
-    box.addEventListener('click', function () {
-        if(turn){
-            box.textContent = 'X'
-        }else{
-            box.textContent = 'O'
-        }
-        movesArray[index] = turn
-        if (movesArray [0] == movesArray[1] &&
-            movesArray [1] == movesArray[2] &&
-            movesArray[0] != null)
-        turn = !turn
-    }, {once : true})
-})
+let boardContainer = Array.from(document.getElementsByClassName("box"));
+
+let turno = true;
+
+let player1 = 3;
+let player2 = 3;
+
+boardContainer.map(
+    (cell) => {
+        cell.addEventListener('click', () => {
+
+            if ((cell.innerHTML === "") && (player1 > 0 || player2 > 0 )){
+                cell.innerHTML = (turno) ? "X" : "O";
+
+                turno = !turno;
+            }
+        })
+    }
+)

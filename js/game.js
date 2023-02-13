14a020f34546756c9863a120c8c012b46c3cb0b0
Ramon <ramon.r.santana@gmail.com>
2023-02-13 20:09:12 +0100

    reestructuración del id en para la combinación ganadora

--- js/game.js
+++ js/game.js
@@ -11,11 +11,11 @@ PlayersName ();
 
 //
 
-
+let running = true;
 let turno = true;
 let playerX = 3;
 let playerO = 3;
-let currentPlayer = true;
+// let currentPlayer = true;
 let boardContainer = Array.from(document.getElementsByClassName("box"));
 
 let miBoardContainer = ["","","","","","","","",""];
@@ -31,27 +31,60 @@ const wincombo = [
     [0, 4, 8],
     [2, 4, 6],
   ];
+   
+//   const compuebaGanador = () =>{
 
- 
-  
-  const compuebaGanador = (miBoardContainer) =>{
+//     if ((miBoardContainer[wincombo[0][0]] ==="x") && (miBoardContainer[wincombo[0][1]] ==="x") && 
+//         console.log(miBoardContainer[wincombo[0][0]] ==="x")
+//     (miBoardContainer[wincombo[0][2]] ==="x")){ 
+//         console.log("X Ha ganado")
+//     }
 
-    console.log(miBoardContainer)
-  }
-  
+//   }
 
 
-boardContainer.map(
+  boardContainer.map(
     (cell,) => {
         cell.addEventListener('click', () => {
 
             if ((cell.innerHTML === "") && (playerX > 0 || playerO > 0 )){
                 cell.innerHTML = (turno) ? "X" : "O";
+                // console.log(cell);
+                // (turno) ? playerX-- : playerO--;
+
                 (turno) ? playerX-- : playerO--;  
                 miBoardContainer[cell.id] = (turno) ? "X" : "O";
-            
+                // compuebaGanador();
                 turno = !turno;
             }
+        
         })
     }
 )
+
+const checkWinner = () => {
+    let roundWon = false;
+
+    for(let i = 0; i < wincombo.length; i++){
+
+        let condition = wincombo[i];
+        let boxA = miBoardContainer[condition[0]];
+        let boxB = miBoardContainer[condition[1]];
+        let boxC = miBoardContainer[condition[2]];
+
+        if(boxA == "" || boxB == "" || boxC == ""){
+            continue;
+        }
+        if(boxA == boxB && boxB == boxC){
+            roundWon = true;
+            break;
+        }
+        console.log(roundWon);
+    }
+
+    if (roundWon){
+        status
+    }
+    }
+    
+    
